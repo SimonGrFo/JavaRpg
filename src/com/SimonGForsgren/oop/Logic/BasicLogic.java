e25c9291891c3bbceb3c3fd68b0f1477abb47b47
SimonGrFo <simgrafor03@gmail.com>
2023-11-30 12:45:29 +0100

    0.2,2

--- src/com/SimonGForsgren/oop/Logic/BasicLogic.java
+++ src/com/SimonGForsgren/oop/Logic/BasicLogic.java
@@ -2,7 +2,7 @@ package com.SimonGForsgren.oop.Logic;
 import java.util.Scanner;
 
 public class BasicLogic {
-    public static boolean isRunning;
+    public static boolean isRunning; //while True, game will run
 
 
 
@@ -15,14 +15,36 @@ public class BasicLogic {
 
     public static void printMenu() {
         System.out.println("Choose an action");
-        System.out.println("(1) FIGHT!");
-        System.out.println("(2) STATUS");
-        System.out.println("(3) EXIT GAME");
+        System.out.println("(1) FIGHT!");       //option 1 to Fight an enemy
+        System.out.println("(2) STATUS");       //option 2 to Check player status
+        System.out.println("(3) QUIT GAME");    //option 3 to Exit the game
     }
 
     public static void GameLoop(){
-        while(isRunning){
+        Scanner scanner = new Scanner(System.in);
 
+        while(isRunning){
+            printMenu();
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();  // Consume the newline character
+
+            switch (choice) {
+                case 1:
+                    // TODO Add fight logic here
+                    break;
+                case 2:
+                    // TODO Add status check logic here
+                    break;
+                case 3:
+                    System.out.println("Exiting");
+                    isRunning = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
         }
+        scanner.close();
     }
+
 }
